33f79e7c4b204519fcf489ab137a5b9b81d87414
Daniel Quack <daniel.q@posteo.de>
2015-11-21 18:45:52 +0100

    Implemented automatic Testing for sorted lists
    - random list with n elements will be created
    - Junit Test that the list is sorted, if not --> error

--- src/test/java/de/unistuttgart/dsaws2015/ex02/p1/SorterTest.java
+++ src/test/java/de/unistuttgart/dsaws2015/ex02/p1/SorterTest.java
@@ -1,32 +1,45 @@
 package de.unistuttgart.dsaws2015.ex02.p1;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
 
 import org.junit.Test;
 
+
+
 public class SorterTest {
-	
+
+public static final int LIST_SIZE = 25;	
+
 	@Test
 	public void testSorter() {
 		SimpleList<Integer> list = new SimpleList<Integer>();
-		
-		// initialize list
-		list.append(5);
-		list.append(7);
-		list.append(100);
-		list.append(1);
-		list.append(25);
-		list.append(45);
-		list.append(13);
-		
+	
+		//generate random list
+		Random randomGenerator = new Random();
+		for (int idx = 0; idx < LIST_SIZE; idx++){
+		      list.append(randomGenerator.nextInt(100));
+		}
+	
+		System.out.print("print random list:\n");
 		list.printList();
-		
-		
+
 		Sorter.selectionSort(list);
 		System.out.print("print sorted list:\n");
 		list.printList();
 		
-	assertTrue(true);
+		//check if list is sorted
+		for (int i = 0; i<list.size()-1; i++) {
+			if(list.get(i).compareTo(list.get(i+1))<=0) {
+				assertTrue(true);
+			}
+			else {
+				System.out.print("list["+(i+1)+"] = "+list.get(i+1)+" < list["+i+"] = "+list.get(i));
+				assertTrue(false);
+			}
+		}
+		
 	}
 
 }
