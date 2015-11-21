0cd217ee736b74b454cb5d07cfbf6f2d587cab96
Daniel Quack <daniel.q@posteo.de>
2015-11-21 16:21:01 +0100

    initial status

---
+++ src/main/java/de/unistuttgart/dsaws2015/ex02/p1/Sorter.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.dsaws2015.ex02.p1;
+
+public class Sorter {
+	
+	public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
+		// TODO
+	}
+
+	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+		// TODO
+	}
+
+	public static <T extends Comparable<T>> void shakerSort(ISimpleList<T> list) {
+		// TODO
+	}
+
+}
---
+++ src/test/java/de/unistuttgart/dsaws2015/ex02/p1/SorterTest.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.dsaws2015.ex02.p1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SorterTest {
+	
+	@Test
+	public void testSorter() {
+		assertTrue(true);
+	}
+
+}
